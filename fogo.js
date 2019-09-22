8cc6f4252da4a13a2a6c58b335b91be657d8a564
leoramos182 <leo.ramos182@hotmail.com>
2019-09-22 17:33:34 -0300

    Criado a estrutura do fogo

--- fogo.js
+++ fogo.js
@@ -1,2 +1,20 @@
-var fogo = "Fogo do Doom"
-console.log(fogo)
+const firePixelsArray = []
+const fireHeight = 2
+const fireWidth = 3
+
+
+function Start(){
+    createFireStructure()
+    console.log(firePixelsArray)
+}
+
+function createFireStructure(){
+    const numberOfPixels = fireHeight * fireWidth 
+    for(let i = 0; i < numberOfPixels; i++){
+        firePixelsArray[i] = 0 ;
+    }
+}
+function calculateFirePropagation(){}
+function renderFire(){}
+
+Start()
